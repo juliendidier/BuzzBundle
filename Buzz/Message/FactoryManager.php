b0746c22f8db9b9a450a65c96d1b11f2fb9c83a6
juliendidier <julien@jdidier.net>
2012-06-02 12:56:45 +0200

    say hello to the message.factory_manager service

---
+++ Buzz/Message/FactoryManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Buzz\Bundle\BuzzBundle\Buzz\Message;
+
+use Buzz\Message\Factory\FactoryInterface;
+
+class FactoryManager
+{
+    protected $factories;
+    protected $configs;
+
+    /**
+     * Set a factory on the collection.
+     *
+     * @param string    $name       The name of the factory
+     * @param Browser   $factory    The factory instance
+     * @param array     $config     The factory configuration
+     *
+     * @return Boolean  Whether the factory is supported
+     */
+    public function set($name, FactoryInterface $factory, array $config = null)
+    {
+        $this->factories[$name] = $factory;
+        $this->configs[$name]   = $config;
+    }
+
+    /**
+     * Returns a factory by name.
+     *
+     * @param string $name The name of the factory
+     *
+     * @return Browser The factory
+     *
+     * @throws BuzzException if the factory can not be retrieved
+     */
+    public function get($name = null)
+    {
+        if (!is_string($name)) {
+            throw new UnexpectedTypeException($name, 'string');
+        }
+
+        if (!$this->has($name)) {
+            throw new BuzzException(sprintf('Buzz message factory with name "%s" not found.', $name));
+        }
+
+        $factory = $this->factories[$name];
+
+        return $factory;
+    }
+
+    /**
+     * Returns whether the given factory is supported.
+     *
+     * @param string $name The name of the factory
+     *
+     * @return Boolean Whether the factory is supported
+     */
+    public function has($name)
+    {
+        if (!is_string($name)) {
+            throw new UnexpectedTypeException($name, 'string');
+        }
+
+        return isset($this->factories[$name]);
+    }
+}
