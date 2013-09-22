f4bce228974e4eca3b3c668d6078a5c7ba872065
Julien DIDIER <julien@didier.io>
2013-09-22 15:16:59 +0200

    clean code

--- Buzz/Buzz.php
+++ Buzz/Buzz.php
@@ -3,16 +3,10 @@
 namespace Buzz\Bundle\BuzzBundle\Buzz;
 
 use Buzz\Bundle\BuzzBundle\Buzz\BrowserManager;
-use Buzz\Bundle\BuzzBundle\Exception\BuzzException;
 
 class Buzz
 {
     protected $browsers;
-
-    public function __construct(BrowserManager $browsers = null, array $config = null)
-    {
-        $this->browsers = $browsers;
-    }
 
     public function getBrowser($name)
     {
